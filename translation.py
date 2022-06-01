677a00697fe8c6fd3551415240e162100d85e329
KDLOVERTECH <103243221+KDLOVERTECH@users.noreply.github.com>
2022-06-01 15:03:46 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,21 @@
+class Translation(object):
+
+      # Start text
+
+      START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/KDLOVERTECH'>Channel Automatic KD bot</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @kannada_movies_groups , @KD_Bot_supporters"             
+
+      #About text
+
+      ABOUT = """
+
+<b>📝 Language:</b> Python 3
+
+<b>🧰 Framework:</b> Pyrogram
+
+<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
+
+<b>📢 Channel:</b> KD BOT UPDATES
+
+<b>👥 Group:</b> KD BOT SUPPORT
+
+<b>🌐Source Code:</b> Press Me 🥰"""
